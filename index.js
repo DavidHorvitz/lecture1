dd1eb5e8079142aa2eb20a0b256837e316c9676d
DavidHorvitz <2205dzh@gmail.com>
2022-12-06 18:02:28 +0200

    test

---
+++ index.js
@@ -0,0 +1,29 @@
+import { writeFile, readFile } from 'node:fs';
+import { exit } from 'node:process';
+// function printName(name,age) {
+//     console.log(`Name ${name}`, `age${age}`);
+// }
+// printName("david", 26);
+// setTimeout((function printName() {
+//     console.log("b");
+// }), 3000);
+// console.log("a");
+const me = {
+    firstName: "david",
+    lastName: "Hor",
+    age: 26
+}
+writeFile("./me.json", JSON.stringify(me), (err) => {
+    if (err) {
+        console.log(err);
+        exit(1);
+    }
+    readFile("./me.json", (err, data) => {
+        if (err) {
+            console.error(err);
+            exit(2);
+        }
+        console.log("%s", data);
+    })
+
+});
